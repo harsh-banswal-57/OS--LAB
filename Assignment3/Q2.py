c39bd7ec954e26faedded0e5a778d23abf3b8b20
Harsh Banswal <23103057@mail.jiit.ac.in>
2025-08-12 09:08:05 +0000

    assignment3

--- Assignment3/Q2.py
+++ Assignment3/Q2.py
@@ -1,16 +1,9 @@
-t = {3: "r", 2: "a", 4:"s", 5 : "h", 1:"h", 6:"k"};
-
-def invertdict(t):
-    return {v:k for k, v in t.items()}
-
-t = invertdict(t)
-
 def sortdic(t): 
     #  key = lambda item : item[1])
     new_dic = dict(sorted(t.items()))
     return new_dic 
 
 
-
+t = {3: "r", 2: "a", 4:"s", 5 : "h", 1:"h", 6:"k"};
 new_dic = sortdic(t)
 print(new_dic)
---
+++ Assignment3/Q3.py
@@ -0,0 +1,10 @@
+li = [1,24,6,7,2,6,54,43,3,2,3,9]
+
+li.sort(key = None, reverse=True)
+
+# print(li)
+
+n = int(input("enter n number"))
+
+for i in range(n):
+    print(li[i])
